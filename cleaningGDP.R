03b519c6281758fa460909e1f308e79b11e9ae7b
cody <codybecker59@gmail.com>
2018-11-16 08:07:44 -0800

    update

--- cleaningGDP.R
+++ cleaningGDP.R
@@ -1,18 +1,13 @@
-#some random levels we don't want
-nonWantedLevels<-c(1,2,4)
-#just the filter part
-df5 <- df4 %>% filter(!as.integer(df4$GeoName) %in% nonWantedLevels)
-levels(df$GeoName)
-droplevels(df5)
+
 unique(df$GeoName)[2:52]
 wantedLevels<- unique(df$GeoName)[2:52]
 wantedLevels<- droplevels(wantedLevels)
-df6 <- df[df$GeoName %in% wantedLevels,]
-df7 <- df6[complete.cases(df6),]
-df8 <- df7 %>% filter(gdp > 8000)
-levels(df8$Description)
+df <- df[df$GeoName %in% wantedLevels,]
+df <- df[complete.cases(df),]
+df <- df %>% 
+  filter(gdp > 8000)
+df <- droplevels(df)
+df <- df %>% 
+  filter(Description == "All industry total")
 
 
-
-
-df10 <- df9 %>% filter(Description == "All industry total")
